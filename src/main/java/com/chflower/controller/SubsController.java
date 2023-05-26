c473e50af8c2a5e0e6d749907eb604ea20df7869
mahanhur <allowbasmh@gmail.com>
2023-05-26 13:12:57 +0900

    mh0526-1

--- src/main/java/com/chflower/controller/SubsController.java
+++ src/main/java/com/chflower/controller/SubsController.java
@@ -1,17 +1,17 @@
 package com.chflower.controller;
 
+import com.chflower.dto.Subs;
 import com.chflower.dto.Subsitem;
+import com.chflower.service.SubsService;
+import com.chflower.service.SubsdetailService;
 import com.chflower.service.SubsitemService;
 import com.chflower.util.FileUploadUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -27,8 +27,13 @@ public class SubsController {
 
     @Autowired
     SubsitemService subsitemService;
+    @Autowired
+    SubsService subsService;
+    @Autowired
+    SubsdetailService subsdetailService;
+
     String dir = "subs";
-    @RequestMapping("")
+    @RequestMapping("/subsitem")
     public String item(Model model){
         List<Subsitem> list = null;
         try {
@@ -38,7 +43,7 @@ public class SubsController {
             throw new RuntimeException(e);
         }
 
-        model.addAttribute("center", dir);
+        model.addAttribute("center", dir+"/subsitem");
         return "index";
     }
     @RequestMapping("/addimpl")
@@ -54,8 +59,32 @@ public class SubsController {
         }
         FileUploadUtil.saveFile(mf,uploadimgdir);
         return "redirect:/subs";
-
     }
+
+
+    @RequestMapping("/subsinfo")
+    public String subsinfo(Model model){
+        List<Subs> list = null;
+        try {
+            list = subsService.get();
+            model.addAttribute("silist",list);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        model.addAttribute("center", dir+"/subsinfo");
+        return "index";
+    }
+    @RequestMapping("/subsinfodel")
+    public String subsinfodel(Model model, Integer subs_id){
+        try {
+            subsService.remove(subs_id);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "redirect:/subs/subsinfo";
+    }
+
 
 
 }
---
+++ src/main/java/com/chflower/mapper/SubsdetailMapper.java
@@ -0,0 +1,12 @@
+package com.chflower.mapper;
+
+import com.chflower.dto.Subsdetail;
+import com.chflower.frame.CHMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface SubsdetailMapper extends CHMapper<Integer, Subsdetail> {
+
+}
