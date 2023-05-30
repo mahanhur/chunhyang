fa2540b07d24be961499769359c48eb7edec13cf
mahanhur <allowbasmh@gmail.com>
2023-05-30 15:57:34 +0900

    mh0530-3

--- src/main/java/com/chflower/mapper/SubsdetailMapper.java
+++ src/main/java/com/chflower/mapper/SubsdetailMapper.java
@@ -5,8 +5,13 @@ import com.chflower.frame.CHMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Mapper
 public interface SubsdetailMapper extends CHMapper<Integer, Subsdetail> {
     public void dateupdate(Subsdetail subsdetail);
+    public List<Subsdetail> get2(Integer subsdetail_id) ;
+
+
 }
---
+++ src/main/java/com/chflower/service/DelinfoService.java
@@ -0,0 +1,42 @@
+package com.chflower.service;
+
+import com.chflower.dto.Delinfo;
+import com.chflower.frame.CHService;
+import com.chflower.mapper.DelinfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DelinfoService implements CHService<Integer, Delinfo> {
+
+    @Autowired
+    DelinfoMapper mapper;
+
+    @Override
+    public void register(Delinfo delinfo) throws Exception {
+        mapper.insert(delinfo);
+    }
+
+    @Override
+    public void remove(Integer del_id) throws Exception {
+        mapper.delete(del_id);
+    }
+
+    @Override
+    public void modify(Delinfo delinfo) throws Exception {
+        mapper.update(delinfo);
+    }
+
+    @Override
+    public Delinfo get(Integer del_id) throws Exception {
+        return mapper.select(del_id);
+    }
+
+    @Override
+    public List<Delinfo> get() throws Exception {
+        return mapper.selectall();
+    }
+
+}
